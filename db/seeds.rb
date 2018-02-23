dd98944f5d2592ca4ce33f1231e9e6acae659310
Caroline Soalhat <csoalhat@gmail.com>
2018-02-22 21:14:23 -0800

    init commit

---
+++ db/seeds.rb
@@ -0,0 +1,19 @@
+ideas = Idea.create(
+  [
+    {
+      title: "A brainstorm app",
+      body: "with cards and images and links and descriptions"
+    },
+    {
+      title: "An artist marketplace",
+      body: "with local artists and currated content"
+    },
+    {
+      title: "A moms buddy",
+      body: "A local app to find moms to help eachother in the area"
+    },
+    {
+      title: "Meal app schedule",
+      body: "Get grocery list for week, weekly meals, recipes and share with family and friends"
+    }
+  ])
